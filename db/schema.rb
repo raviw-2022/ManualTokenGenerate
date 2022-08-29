d8a76d50437aed392f2ac2f490e51eb0322fa71e
raviw-2022 <ravi.s@webkorps.com>
2022-08-30 02:27:04 +0530

    Error Solve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_164818) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_201614) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "string"
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_164818) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "users"
 end
