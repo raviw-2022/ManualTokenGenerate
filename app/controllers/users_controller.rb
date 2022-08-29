b816a3abe271113084f198214d1b5e5491a8687d
raviw-2022 <ravi.s@webkorps.com>
2022-08-30 00:50:10 +0530

    Add login action to authenticate the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,20 @@ class UsersController < ApplicationController
       token = encode_token({user_id: @user.id})
       render json: {user: @user, token: token}, status: :ok
     else
-      render json: {error: "Invalid username or password"}, status: :unprocesssable_entity
+      render json: {error: "Invalid username or password"}, status: :unprocessable_entity
     end
   end
+
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(user_params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user,token: token}, status: :ok
+    else
+      render json: {error: "Invalid username"}, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
